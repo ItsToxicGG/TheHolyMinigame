690cb9130d3feaf27ef3091b8e7b484dfdb4e09d
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-04 01:05:49 +0200

    Some chnages not big

--- src/ToxicGG/TheUnHoly/Holy.php
+++ src/ToxicGG/TheUnHoly/Holy.php
@@ -1,2 +1,22 @@
 <?php
+
+namespace ToxicGG\TheUnHoly;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use ToxicGG\TheUnHoly\Commands\HolyCommand;
+
+class Holy extends PluginBase{
+
+  public function onLoad(): void{
+    $this->getLogger()->info("A Demon is tryna latch into the server");
+  }
+  
+  public function onEnable(): void{
+    $this->getLogger()->info("A Demon has been enabled in your server");
+  }
+  
+  public function onDisable(): void{
+    $this->getLogger()->info("A Demon got burned from pure holy water");
+  }
 } 
